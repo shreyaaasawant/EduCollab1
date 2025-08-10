43ef0c12497767b63ccf403766c62f3f97902121
shreya sawant <142585997+shreyaaasawant@users.noreply.github.com>
2025-08-10 17:52:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = socketIO(server, { cors: { origin: '*' } });
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+io.on('connection', (socket) => {
+  socket.on('joinGroup', (groupId) => {
+    socket.join(groupId);
+  });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+  socket.on('chatMessage', ({ groupId, user, text }) => {
+    io.to(groupId).emit('chatMessage', { user, text });
+  });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  socket.on('joinCode', (groupId) => {
+    socket.join('code_' + groupId);
+  });
+
+  socket.on('codeUpdate', ({ groupId, code }) => {
+    io.to('code_' + groupId).emit('codeUpdate', code);
+  });
+});
+
+server.listen(5000, () => console.log('Server listening on port 5000'));
